db79800a09ce65f7c537b9f33f3d0153dd57112a
Linh <Redmercy161201@gmail.com>
2023-05-04 17:29:17 +0700

    Header

--- react/src/components/Header/index.jsx
+++ react/src/components/Header/index.jsx
@@ -1,14 +1,405 @@
 import classNames from "classnames/bind";
-import styles from "./Header.module.scss"
+import styles from "./Header.module.scss";
+import { Link } from "react-router-dom";
+import logo from "../../assets/Logo.png";
+import searchLogo from "../../assets/Search.png";
+import cartLogo from "../../assets/Cart.png";
+import userLogo from "../../assets/User.png";
+import 'reactjs-popup/dist/index.css';
+import Popup from 'reactjs-popup';
 
 const cx = classNames.bind(styles);
 
 function Header() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
-        
-      </div>
+        <div className={cx('inner1')}>
+          <Link to="/" className={cx('logo-header', 'cover-img')}>
+            <div className={cx('w100')}>
+              <img
+                className={cx('logo')}
+                src={logo}
+                alt=""
+              />
+            </div>
+          </Link>
+          <div className={cx('content')}>
+            <div className={cx('search-cart-login')}>
+              <div className={cx('search')}>
+                <input
+                  className={cx('search-input')}
+                  placeholder="Search name laptop you want..."
+                  spellCheck={false}
+                />
+                <button className={cx('search-btn')} onClick={handleSubmit}>
+                  <img
+                    className={cx('search-logo')}
+                    src={searchLogo}
+                    alt=""
+                  />
+                </button>
+              </div>
+              <div className={cx('cart')}>
+                <img
+                    className={cx('cart-logo')}
+                    src={cartLogo}
+                    alt=""
+                  />
+              </div>
+              <div className={cx('user')}>
+                <img
+                    className={cx('user-logo')}
+                    src={userLogo}
+                    alt=""
+                  />
+                <Link to="/login">Login</Link>
+              </div>
+            </div>
+            <nav className={cx('navi')}>
+              <Popup
+                trigger={
+                  <Link className={cx('text')}>
+                    Laptop brand
+                  </Link>
+                }
+              >
+                <div>
+                  <Link className={cx('text')}>
+                    MacBook
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    HP
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    DELL
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    ASUS
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Lenovo
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Acer
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Xiaomi
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Microsoft Surface
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    LG
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    HUAWEI
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    MSI
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    GIGABYTE
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Fujitsu
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Intel
+                  </Link>
+                </div>
+              </Popup>
+
+              <Popup
+                trigger={
+                  <Link className={cx('text')}>
+                    Price
+                  </Link>
+                }
+              >
+                <div>
+                  <Link className={cx('text')}>
+                    Dưới 10 triệu
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Từ 10-15 triệu
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Từ 15-20 triệu
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Từ 20-25 triệu
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Trên 25 triệu
+                  </Link>
+                </div>
+              </Popup>
+
+              <Popup
+                trigger={
+                  <Link className={cx('text')}>
+                    CPU
+                  </Link>
+                }
+              >
+                <div>
+                  <Link className={cx('text')}>
+                    Intel celeron
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Intel pentium
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Intel core i3
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Intel core i5
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Intel core i7
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Amd ryzen 3
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Amd ryzen 5
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Amd ryzen 7
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Amd ryzen 9
+                  </Link>
+                </div>
+              </Popup>
+
+              <Popup
+                trigger={
+                  <Link className={cx('text')}>
+                    Monitor
+                  </Link>
+                }
+              >
+                <div>
+                  <Link className={cx('text')}>
+                    Khoảng 13 inch
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Khoảng 14 inch
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Khoảng 15 inch
+                  </Link>
+                </div>
+              </Popup>
+
+              <Popup
+                trigger={
+                  <Link className={cx('text')}>
+                    Ram
+                  </Link>
+                }
+              >
+                <div>
+                  <Link className={cx('text')}>
+                    4GB
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    8GB
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    16GB
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    32GB
+                  </Link>
+                </div>
+              </Popup>
+
+              <Popup
+                trigger={
+                  <Link className={cx('text')}>
+                    Graphics
+                  </Link>
+                }
+              >
+                <div>
+                  <Link className={cx('text')}>
+                    NVIDIA GeForce Series
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Card Onboard
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    AMD Radeon Series
+                  </Link>
+                </div>
+              </Popup>
+
+              <Popup
+                trigger={
+                  <Link className={cx('text')}>
+                    Hardware
+                  </Link>
+                }
+              >
+                <div>
+                  <Link className={cx('text')}>
+                    SSD 128GB
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    SSD 256GB
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    SSD 512GB
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    SSD 1TB
+                  </Link>
+                </div>
+              </Popup>
+
+              <Popup
+                trigger={
+                  <Link className={cx('text')}>
+                    Special features
+                  </Link>
+                }
+              >
+                <div>
+                  <Link className={cx('text')}>
+                    Hỗ trợ công nghệ Optane
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    CPU Intel
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    CPU Intel Gen 13
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Card RTX Series 4000
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Sử dụng tấm nền IPS
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Màn hình cảm ứng
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Bảo vệ trẻ em
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    CPU AMD
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Windows bản quyền
+                  </Link>
+                </div>
+                <div>
+                  <Link className={cx('text')}>
+                    Mở khóa vân tay
+                  </Link>
+                </div>
+              </Popup>
+
+              <Link className={cx('text')} to="/about">
+                  About Us
+              </Link>
+          </nav>
+          </div>
+        </div>
+     </div>
     </header>
    );
 }
